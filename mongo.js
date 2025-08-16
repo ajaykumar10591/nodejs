2faea59ceacde91758d557fb631bcf3ee4433ca6
Ajay Kumar <ajay.kumar1@kellton.com>
2025-08-16 16:07:25 +0530

    section  8 practice done

--- mongo.js
+++ mongo.js
@@ -1,40 +1,41 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const url = "mongodb+srv://ajaykumar2:Duqpt804Svh5IKXt@cluster0.5dxyfl5.mongodb.net/products_test?retryWrites=true&w=majority&appName=Cluster0";
+const url = 'mongodb+srv://ajaykumar2:Duqpt804Svh5IKXt@cluster0.5dxyfl5.mongodb.net/products_test?retryWrites=true&w=majority&appName=Cluster0'
 
 const createProduct = async (req, res, next) => {
   const newProduct = {
     name: req.body.name,
     price: req.body.price
   };
-  
   const client = new MongoClient(url);
 
   try {
     await client.connect();
     const db = client.db();
-    await db.collection('products').insertOne(newProduct);
-    res.json(newProduct);
+    const result = db.collection('products').insertOne(newProduct);
   } catch (error) {
-    res.json({message: 'Could not store data.'});
-  } finally {
-    await client.close();
-  }
+    return res.json({message: 'Could not store data.'});
+  };
+  client.close();
+
+  res.json(newProduct);
 };
 
 const getProducts = async (req, res, next) => {
   const client = new MongoClient(url);
 
+  let products;
+
   try {
     await client.connect();
     const db = client.db();
-    const products = await db.collection('products').find().toArray();
-    res.json(products);
+    products = await db.collection('products').find().toArray();
   } catch (error) {
-    res.json({message: 'Could not retrieve products.'});
-  } finally {
-    await client.close();
-  }
+    return res.json({message: 'Could not retrieve products.'});
+  };
+  client.close();
+
+  res.json(products);
 };
 
 exports.createProduct = createProduct;
